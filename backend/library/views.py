116e2f113f03e3556c3dbac83ab6e99c606a9d61
monsieurswag <mael.joumier@intuitem.com>
2024-08-21 11:47:35 +0200

    Remove incorrect rationale value the CJIS library

--- backend/library/views.py
+++ backend/library/views.py
@@ -126,8 +126,7 @@ class StoredLibraryViewSet(BaseModelViewSet):
                     status=HTTP_400_BAD_REQUEST,
                 )  # This can cause translation issues
             return Response({"status": "success"})
-        except Exception as e :
-            raise e
+        except :
             return Response(
                 {"error": "Failed to load library"},  # This must translated
                 status=HTTP_422_UNPROCESSABLE_ENTITY,
