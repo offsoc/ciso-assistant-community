3ea414e87ba5a27e0e7ea4615cd19fe425e5a2c3
Nassim Tabchiche <n.tabchiche2@gmail.com>
2024-12-23 17:34:03 +0100

    Add content slot to Tile component

--- frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/Tile.svelte
+++ frontend/src/routes/(app)/(internal)/ebios-rm/[id=uuid]/Tile.svelte
@@ -33,78 +33,80 @@
 				{/if}
 			</div>
 		</div>
-		{#if meta}
-			<div class="flex mx-auto">
-				<div>
-					<ol class="relative text-gray-500 border-s border-gray-200">
-						{#each meta as step, i}
-							<li class="flex flex-row justify-between mb-10 ms-6">
-								{#if createRiskAnalysis && i == 0}
-									<slot name="addRiskAnalysis"></slot>
-								{:else}
-									<Anchor
-										href={step.href}
-										prefixCrumbs={[{ label: safeTranslate(`ebiosWs${workshop}`) }]}
-										class="hover:text-purple-800"
+		<slot name="content">
+			{#if meta}
+				<div class="flex mx-auto">
+					<div>
+						<ol class="relative text-gray-500 border-s border-gray-200">
+							{#each meta as step, i}
+								<li class="flex flex-row justify-between mb-10 ms-6">
+									{#if createRiskAnalysis && i == 0}
+										<slot name="addRiskAnalysis"></slot>
+									{:else}
+										<Anchor
+											href={step.href}
+											prefixCrumbs={[{ label: safeTranslate(`ebiosWs${workshop}`) }]}
+											class="hover:text-purple-800"
+										>
+											{#if step.status == 'done'}
+												<span
+													class="absolute flex items-center justify-center w-8 h-8 bg-success-200 rounded-full -start-4 ring-4 ring-white"
+												>
+													<i class="fa-solid fa-check" />
+												</span>
+											{:else}
+												<span
+													class="absolute flex items-center justify-center w-8 h-8 bg-surface-200 rounded-full -start-4 ring-4 ring-white"
+												>
+													<i class="fa-solid fa-clipboard-check" />
+												</span>
+											{/if}
+											<h3 class="font-medium leading-tight">{m.activity()} {i + 1}</h3>
+											<p class="text-sm">{step.title}</p>
+										</Anchor>
+									{/if}
+									<button
+										class="btn bg-initial"
+										data-testid="sidebar-more-btn"
+										use:popup={{
+											event: 'click',
+											target: `popupStep-${workshop}.${i + 1}`,
+											placement: 'top'
+										}}><i class="fa-solid fa-ellipsis-vertical" /></button
 									>
-										{#if step.status == 'done'}
-											<span
-												class="absolute flex items-center justify-center w-8 h-8 bg-success-200 rounded-full -start-4 ring-4 ring-white"
-											>
-												<i class="fa-solid fa-check" />
-											</span>
-										{:else}
-											<span
-												class="absolute flex items-center justify-center w-8 h-8 bg-surface-200 rounded-full -start-4 ring-4 ring-white"
-											>
-												<i class="fa-solid fa-clipboard-check" />
-											</span>
-										{/if}
-										<h3 class="font-medium leading-tight">{m.activity()} {i + 1}</h3>
-										<p class="text-sm">{step.title}</p>
-									</Anchor>
-								{/if}
-								<button
-									class="btn bg-initial"
-									data-testid="sidebar-more-btn"
-									use:popup={{
-										event: 'click',
-										target: `popupStep-${workshop}.${i + 1}`,
-										placement: 'top'
-									}}><i class="fa-solid fa-ellipsis-vertical" /></button
-								>
-								<div
-									class="card whitespace-nowrap bg-white py-2 w-fit shadow-lg space-y-1"
-									data-testid="sidebar-more-panel"
-									data-popup="popupStep-{workshop}.{i + 1}"
-								>
-									<form
-										action="/ebios-rm/{$page.params.id}?/changeStepState"
-										method="POST"
-										use:enhance={() => {
-											return async () => {
-												if (step.status !== 'done') step.status = 'done';
-												else step.status = 'in_progress';
-											};
-										}}
+									<div
+										class="card whitespace-nowrap bg-white py-2 w-fit shadow-lg space-y-1"
+										data-testid="sidebar-more-panel"
+										data-popup="popupStep-{workshop}.{i + 1}"
 									>
-										<input type="hidden" name="workshop" value={workshop} />
-										<input type="hidden" name="step" value={i + 1} />
-										{#if step.status === 'done'}
-											<input type="hidden" name="status" value="in_progress" />
-											<button type="submit" class="btn bg-initial">{m.markAsInProgress()}</button>
-										{:else}
-											<input type="hidden" name="status" value="done" />
-											<button type="submit" class="btn bg-initial">{m.markAsDone()}</button>
-										{/if}
-									</form>
-								</div>
-							</li>
-						{/each}
-					</ol>
+										<form
+											action="/ebios-rm/{$page.params.id}?/changeStepState"
+											method="POST"
+											use:enhance={() => {
+												return async () => {
+													if (step.status !== 'done') step.status = 'done';
+													else step.status = 'in_progress';
+												};
+											}}
+										>
+											<input type="hidden" name="workshop" value={workshop} />
+											<input type="hidden" name="step" value={i + 1} />
+											{#if step.status === 'done'}
+												<input type="hidden" name="status" value="in_progress" />
+												<button type="submit" class="btn bg-initial">{m.markAsInProgress()}</button>
+											{:else}
+												<input type="hidden" name="status" value="done" />
+												<button type="submit" class="btn bg-initial">{m.markAsDone()}</button>
+											{/if}
+										</form>
+									</div>
+								</li>
+							{/each}
+						</ol>
+					</div>
 				</div>
-			</div>
-		{/if}
+			{/if}
+		</slot>
 		<div class="justify-end flex"></div>
 	</div>
 </div>
